f88e3e53de5927fe049c5a3fabe593ebab43e7c7
zwhahn <117476820+zwhahn@users.noreply.github.com>
2026-01-21 17:22:04 -0500

    style: add navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router";
+import styles from "../styleModules/navbar.module.css";
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className={styles.navbar}>
       <Link to="">Home</Link>
 
       <Link to="shop">Shop</Link>
