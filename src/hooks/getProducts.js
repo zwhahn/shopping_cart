81703a23066686ba80a1f692aa9dc61ec36938ef
zwhahn <117476820+zwhahn@users.noreply.github.com>
2026-01-22 16:43:25 -0500

    feat: create hook to FakestoreAPI

---
+++ src/hooks/getProducts.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+const useProducts = () => {
+  const FAKESTORE_URL = "https://fakestoreapi.com/products";
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch(FAKESTORE_URL);
+        const data = await response.json();
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  return { products, loading, error };
+};
+
+export default useProducts;
