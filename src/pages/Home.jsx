596bf851b1260cb1ccf0dcdb72cc3e3ddce42cdf
zwhahn <117476820+zwhahn@users.noreply.github.com>
2026-01-27 16:15:19 -0500

    style: add welcome banner
    Unstyled text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import styles from "../styleModules/home.module.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className={styles.homeContainer}>
+        <h1>Welcome to My Shop</h1>
+        <p>
+          All items are sourced locally from{" "}
+          <a href="https://fakestoreapi.com/">FakestoreAPI</a>{" "}
+          and all sales are not final. Enjoy Shopping!
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default Home;
